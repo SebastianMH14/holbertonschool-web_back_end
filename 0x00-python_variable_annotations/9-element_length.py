8cf29e8b42aebc9d76e398e3e099e72632a62eb2
SebastianMH14 <sebasmh2002@hotmail.com>
2022-01-31 15:58:31 -0500

    add task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-"""Let's duck type an iterable object"""
+"""Let's duck type an iterable object
+return values with the appropriate types"""
 import typing
 
 
 def element_length(
         lst: typing.Iterable[typing.Sequence]) -> \
         typing.List[typing.Tuple[typing.Sequence, int]]:
+    """return values with the appropriate types"""
     return [(i, len(i)) for i in lst]
