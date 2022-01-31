cd40d0f2936222abe28156f3e687030477e37e46
SebastianMH14 <sebasmh2002@hotmail.com>
2022-01-31 17:57:15 -0500

    add task 102

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Use mypy to validate the following piece
+of code and apply any necessary changes."""
+import typing
+
+
+def zoom_array(lst: typing.Tuple, factor: int = 2) -> typing.List:
+    zoomed_in: typing.List[typing.Any] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
