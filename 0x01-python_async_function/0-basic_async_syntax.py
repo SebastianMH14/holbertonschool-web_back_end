6e32ab577b8ee4278667a00a5e32699ec71561f5
SebastianMH14 <sebasmh2002@hotmail.com>
2022-02-01 13:28:37 -0500

    fix pycode

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,11 +3,11 @@
 in an integer argument (max_delay, with
 a default value of 10) named wait_random
 that waits for a random delay between 0 and
-max_delay (included and float value) seconds 
+max_delay (included and float value) seconds
 and eventually returns it."""
 import random
 
 
-async def wait_random(max_delay = 10):
+async def wait_random(max_delay=10):
     """takes in an integer argument"""
     return random.uniform(0, max_delay)
