9c54f6883609e153640afed75945b34eab01aafe
SebastianMH14 <sebasmh2002@hotmail.com>
2022-02-01 13:27:56 -0500

    Add task 0 and readme

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""asynchronous coroutine that takes
+in an integer argument (max_delay, with
+a default value of 10) named wait_random
+that waits for a random delay between 0 and
+max_delay (included and float value) seconds 
+and eventually returns it."""
+import random
+
+
+async def wait_random(max_delay = 10):
+    """takes in an integer argument"""
+    return random.uniform(0, max_delay)
