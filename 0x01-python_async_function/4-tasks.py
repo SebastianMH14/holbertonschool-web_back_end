57828ff3f9d0af4c9470a8a77a40bad30f90956c
SebastianMH14 <sebasmh2002@hotmail.com>
2022-02-01 23:18:12 -0500

    add task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""ask_wait_n. The code is nearly
+identical to wait_n except task_wait_random
+is being called."""
+import asyncio
+import typing
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
+    """return the list of all the delays (float values)."""
+    results: typing.List[float] = []
+    for delay in range(n):
+        results.append(wait_random(max_delay))
+    response = await asyncio.gather(*results)
+    return sorted(response)
