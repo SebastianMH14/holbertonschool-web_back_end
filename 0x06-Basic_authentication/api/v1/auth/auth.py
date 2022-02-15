7a3e4e5551786835b2074fd5e78f47acf2dafb97
SebastianMH14 <sebasmh2002@hotmail.com>
2022-02-15 10:10:43 -0500

    add new API

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Auth class for authentication"""
+from flask import request as req
+from typing import List, TypeVar
+
+
+class Auth:
+
+    def __init__(self):
+        """constructor"""
+        pass
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Returns True if the path is not in the list of strings excluded_paths"""
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        else:
+            for paths in excluded_paths:
+                if paths.replace('/', '') == path.replace('/', ''):
+                    return False
+                else:
+                    return True
+
+    def authorization_header(self, request=None) -> str:
+        """If request doesn't contain the header key Authorization,
+        returns None; Otherwise, return the value of the 
+        header request Authorization"""
+        if request is None:
+            return None
+        else:
+            header_auth = request.headers.get('Authorization')
+            if header_auth is None:
+                return None
+            else:
+                return header_auth
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """request will be the Flask request object"""
+        return None
