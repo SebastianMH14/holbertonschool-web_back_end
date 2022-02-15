89cb98a4da7ce3c50082c2998983a7eee31254bb
SebastianMH14 <sebasmh2002@hotmail.com>
2022-02-15 11:36:52 -0500

    Add more docstring

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""Auth class for authentication"""
+"""Auth class for basic authentication and authorization"""
 from flask import request
 from typing import List, TypeVar
 
 
 class Auth:
-    """Auth class for authentication"""
+    """Auth class for authentication and authorization"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Returns True if the path is not in the list of strings excluded_paths"""
