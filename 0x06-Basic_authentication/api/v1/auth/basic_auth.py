fc11599638ee7b761e08a365a4667017010dd0be
SebastianMH14 <sebasmh2002@hotmail.com>
2022-02-15 15:42:08 -0500

    Update class basic_auth with new method

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,14 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """class BasicAuth to basic authentication"""
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """returns the Base64 part of the Authorization
+        header for a Basic Authentication"""
+        if authorization_header is None or type(
+                authorization_header) is not str or authorization_header.find(
+                    'Basic ') == -1:
+            return None
+        else:
+            return authorization_header.lstrip('Basic ')
