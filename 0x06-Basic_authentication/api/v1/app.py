6cb2cec614a22fac2d8381f244c77038823646e0
SebastianMH14 <sebasmh2002@hotmail.com>
2022-02-15 14:49:03 -0500

    update app add method before_request

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -7,12 +7,30 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+
+if getenv('AUTH_TYPE') == 'auth':
+    auth = Auth()
+
+
+@app.before_request
+def before_request():
+    """define the authentication method"""
+    if auth is not None:
+        path = request.path
+        check = auth.require_auth(path, [
+            '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'])
+        if check is True:
+            if auth.authorization_header(request) is None:
+                abort(401)
+            elif auth.current_user(request) is None:
+                abort(403)
 
 
 @app.errorhandler(404)
