a0df1560ad0294ded593cdbd5de55557e6e48929
SebastianMH14 <sebasmh2002@hotmail.com>
2022-02-15 11:39:09 -0500

    Fix pycodestyle

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,8 @@ class Auth:
     """Auth class for authentication and authorization"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Returns True if the path is not in the list of strings excluded_paths"""
+        """Returns True if the path is not in the
+        list of strings excluded_paths"""
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
         else:
@@ -20,7 +21,7 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """If request doesn't contain the header key Authorization,
-        returns None; Otherwise, return the value of the 
+        returns None; Otherwise, return the value of the
         header request Authorization"""
         if request is None:
             return None
