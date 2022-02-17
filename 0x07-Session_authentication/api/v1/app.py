8d8138bb9b930bc9f4ae73275c18b24d2a2c94bf
SebastianMH14 <sebasmh2002@hotmail.com>
2022-02-17 15:06:48 -0500

    update method @app.before_request

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -31,9 +31,11 @@ def before_request():
     if auth is not None:
         path = request.path
         check = auth.require_auth(path, [
-            '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'])
+            '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'])
         if check is True:
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is None \
+                    and auth.session_cookie(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
