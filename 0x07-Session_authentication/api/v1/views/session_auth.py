755e4094892f420108437e448d99b8969209111b
SebastianMH14 <sebasmh2002@hotmail.com>
2022-02-17 19:31:34 -0500

    Fix status_error codes

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -11,17 +11,17 @@ def login():
     """etrieve the User instance based on the email"""
     email = request.form.get('email')
     password = request.form.get('password')
-    if email is None or email == "":
+    if email is None or email is False:
         return jsonify({"error": "email missing"}), 400
-    if password is None or password == "":
+    if password is None or password is False:
         return jsonify({"error": "password missing"}), 400
 
     search = User.search({'email': email})
     if len(search) == 0:
-        return jsonify({"error": "no user found for this email"}), 400
+        return jsonify({"error": "no user found for this email"}), 404
 
     if search[0].is_valid_password(password) is False:
-        return jsonify({"error": "wrong password"}), 400
+        return jsonify({"error": "wrong password"}), 401
 
     from api.v1.app import auth
     session_id = auth.create_session(search[0].id)
