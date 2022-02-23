589575e35958fb5226d85903c958316efa546d04
SebastianMH14 <sebasmh2002@hotmail.com>
2022-02-23 13:24:39 -0500

    add db class

---
+++ 0x08-user_authentication_service/db.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine, text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email, hashed_password):
+        """two required string arguments:
+        email and hashed_password,
+        and returns a User object"""
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
+
+    def find_user_by(self, **kwargs):
+        query = "SELECT * FROM users"
+        i = 0
+        for key, value in kwargs.items():
+            if i == 0:
+                query += " WHERE "
+            else:
+                query += " AND "
+            query += "{}='{}'".format(key, value)
+            i += 1
+        query += ";"
+        users = self._session.query(User).from_statement(
+            text(query))
+        return users[0]
