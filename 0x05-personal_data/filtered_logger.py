252835a31052426fac67ae8d621717c9ed3833f4
SebastianMH14 <sebasmh2002@hotmail.com>
2022-02-23 21:58:35 -0500

    fix

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -3,22 +3,6 @@
 that returns the log message obfuscated"""
 from typing import List
 import re
-import logging
-
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-
-    def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
 
 
 def filter_datum(fields: List[str],
