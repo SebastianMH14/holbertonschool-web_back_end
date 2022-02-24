d15546e7856659da7ffd1dd84107cc5f8e55d504
SebastianMH14 <sebasmh2002@hotmail.com>
2022-02-24 12:25:02 -0500

    add task 4

---
+++ 0x08-user_authentication_service/auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""auth class for authentication"""
+import bcrypt
+
+
+def _hash_password(password: str):
+    """returned bytes is a salted hash of the input password"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
