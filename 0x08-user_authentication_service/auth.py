784db0384bce69d985f4270924dd5048bf1f83c9
SebastianMH14 <sebasmh2002@hotmail.com>
2022-02-24 12:39:31 -0500

    add task auth

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 """auth class for authentication"""
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """save the user to the database
+        using self._db and return the User object"""
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError('User {} already exists'.format(email))
+        except NoResultFound:
+            passw = _hash_password(password)
+            return self._db.add_user(email, passw)
 
 
 def _hash_password(password: str) -> bytes:
