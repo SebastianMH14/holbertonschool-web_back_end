e1b4397ae7595dd23ed7aa3ef8ba0388f0a38e82
SebastianMH14 <sebasmh2002@hotmail.com>
2022-02-24 10:35:51 -0500

    Fix add_user methood

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -32,7 +32,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """two required string arguments:
         email and hashed_password,
         and returns a User object"""
