9730f1fa4b6992be974e23b7a5bf4cd2da933355
SebastianMH14 <sebasmh2002@hotmail.com>
2022-02-23 21:53:41 -0500

    add task 0

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""function called filter_datum
+that returns the log message obfuscated"""
+from typing import List
+import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
+
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """ use a regex to replace occurrences of certain field values
+    use re.sub to perform the substitution with a single regex."""
+    for field in fields:
+        message = re.sub(r'({}=).*?{}'.format(field, separator),
+                         r'\1{}{}'.format(redaction, separator), message)
+    return message
