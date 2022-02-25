0157f464b880dfaccfae77d370982a24f2dc570e
SebastianMH14 <sebasmh2002@hotmail.com>
2022-02-24 19:01:42 -0500

    add new method

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -49,6 +49,20 @@ class Auth:
         except NoResultFound:
             return None
 
+    def get_user_from_session_id(self, session_id) -> User:
+        """
+        If the session ID is None or no user is found,
+        return None. Otherwise return the corresponding user.
+        """
+        if session_id is None:
+            return None
+        else:
+            try:
+                user = self._db.find_user_by(session_id=session_id)
+                return user
+            except NoResultFound:
+                return None
+
 
 def _hash_password(password: str) -> bytes:
     """returned bytes is a salted hash of the input password"""
