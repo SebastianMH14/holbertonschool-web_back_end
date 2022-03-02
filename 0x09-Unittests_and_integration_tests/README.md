f8c24a1a1a4ee05c79a1028f46070695ba905148
SebastianMH14 <sebasmh2002@hotmail.com>
2022-03-02 15:32:18 -0500

    add firts test and readme

---
+++ 0x09-Unittests_and_integration_tests/README.md
@@ -0,0 +1,6 @@
+# 0x09. Unittests and Integration Tests
+
+<ul>
+<li>The difference between unit and integration tests.</li>
+<li>Common testing patterns such as mocking, parametrizations and fixtures</li>
+</ul>
---
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""test access_nested_map function"""
+from utils import access_nested_map
+from parameterized import parameterized
+import unittest
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """class for test"""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, map, path, expected):
+        """test the result"""
+        result = access_nested_map(map, path)
+        self.assertEqual(result, expected)
