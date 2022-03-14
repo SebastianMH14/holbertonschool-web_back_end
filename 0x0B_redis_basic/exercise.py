a07827188b32d23fe6263ad9244b61631742baa5
SebastianMH14 <sebasmh2002@hotmail.com>
2022-03-14 16:23:15 -0500

    add task 0

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Writing strings to Redis
+"""
+import redis
+import uuid
+from typing import TypeVar
+from collections.abc import Callable
+
+
+T = TypeVar('T', str, bytes, int, float)
+
+
+class Cache:
+    """store an instance of the Redis client
+    as a private variable named _redis"""
+
+    def __init__(self):
+        """constructor de clase"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: T) -> str:
+        """store the input data in Redis
+        using the random key and return the key"""
+        key = str(uuid.uuid4())
+        self._redis.mset({key: data})
+        return key
+
+    def get(self, key: str, fn):
+        """convert the data back to the desired format"""
+        result = self._redis.get(key)
+        if result:
+            fn.g
+        return result
+
+    def get_str(self, str_utf8: str) -> str:
+        """convert utf8 to string"""
+        return str(str_utf8.decode('utf8'))
+
+    def get_int(self, int_utf8) -> int:
+        """convert utf8 to int"""
+        return int(int_utf8.decode('utf8'))
