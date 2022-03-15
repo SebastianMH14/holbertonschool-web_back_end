a8eead10f3b27fbc9fa4d453fa46ae536f6ab9f6
SebastianMH14 <sebasmh2002@hotmail.com>
2022-03-15 17:42:23 -0500

    Add decorator to store

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -28,6 +28,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """store the input data in Redis
         using the random key and return the key"""
