a4a255c0af98ed152941643b859e13f92a2495fd
SebastianMH14 <sebasmh2002@hotmail.com>
2022-03-17 11:36:35 -0500

    Fix task 3

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,10 @@
 -- script that lists all bands with Glam rock as their main style
 SELECT
-  band_name, iFNULL(split, 2022) - formed AS lifespan
+  band_name,
+  iFNULL(split, 2022) - formed AS lifespan
 from
   metal_bands
 where
-  style LIKE "%Glam rock%";
+  style LIKE "%Glam rock%"
+ORDER BY
+  lifespan DESC;
