e1f28b66b8cea01b285ae81086a4ab490af79159
SebastianMH14 <sebasmh2002@hotmail.com>
2022-03-22 17:07:44 -0500

    fix task 10

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -5,4 +5,4 @@ document based on the name"""
 
 def update_topics(mongo_collection, name, topics):
     """update topics of a school"""
-    mongo_collection.update_one({"name": name}, {'$set': {'topics':topics}})
+    mongo_collection.update_many({"name": name}, {'$set': {'topics':topics}})
