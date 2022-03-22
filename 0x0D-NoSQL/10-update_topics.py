1ed0dc2dad89cb145667039114ba36e3c40072b2
SebastianMH14 <sebasmh2002@hotmail.com>
2022-03-22 17:05:46 -0500

    add task 10

---
+++ 0x0D-NoSQL/10-update_topics.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""function that changes all topics of a school
+document based on the name"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """update topics of a school"""
+    mongo_collection.update_one({"name": name}, {'$set': {'topics':topics}})
