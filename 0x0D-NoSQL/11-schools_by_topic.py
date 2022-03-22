7f9d09774a7a1a4a7624ca4c5e9dc3e5052f05ee
SebastianMH14 <sebasmh2002@hotmail.com>
2022-03-22 17:13:15 -0500

    add task 11

---
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" function that returns the list of school"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """find topic"""
+    return mongo_collection.find({"topic":topic})
