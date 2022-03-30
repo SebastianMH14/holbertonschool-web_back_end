804351e3ffcd21d45d8fe9575a9e5dc4ebbae6bf
SebastianMH14 <sebasmh2002@hotmail.com>
2022-03-29 20:00:29 -0500

    Fix task 9

--- 0x0F-ES6_promise/9-try.js
+++ 0x0F-ES6_promise/9-try.js
@@ -3,7 +3,7 @@ export default function guardrail(mathFunction) {
   try {
     queue.push(mathFunction());
   } catch (err) {
-    queue.push(mathFunction());
+    queue.push(err.toString());
   }
   queue.push('Guardrail was processed');
   return queue;
