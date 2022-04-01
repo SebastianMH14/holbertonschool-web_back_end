06db750a1ca3992b0ba59ae4d39a4d355178fdac
SebastianMH14 <sebasmh2002@hotmail.com>
2022-03-31 19:01:14 -0500

    Add task 4

---
+++ 0x10-ES6_classes/4-pricing.js
@@ -0,0 +1,49 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount === 'number') {
+      this._amount = amount;
+    } else {
+      throw TypeError('Amount must be a number');
+    }
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    } else {
+      throw TypeError('Currency must be a Currency object');
+    }
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount === 'number') {
+      this._amount = amount;
+    } else {
+      throw TypeError('Amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    } else {
+      throw TypeError('Currency must be a Currency object');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate)
+  {
+    return amount * conversionRate;
+  }
+}
