768edc3223337c74bbf3bb9df57a9b63db2ee3c7
SebastianMH14 <sebasmh2002@hotmail.com>
2022-04-05 10:36:55 -0500

    fix task 6

--- 0x11-ES6_data_manipulation/6-set.js
+++ 0x11-ES6_data_manipulation/6-set.js
@@ -1,7 +1,7 @@
-export default function setFromArray(Array) {
-  if (!Array.isArray(Array)) {
+export default function setFromArray(array) {
+  if (!Array.isArray(array)) {
     return 0;
   }
-  const set = new Set(Array);
+  const set = new Set(array);
   return set;
 }
