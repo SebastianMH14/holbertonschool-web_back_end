eb6c89bd174eac48d78767eef70b4c41bd1b28b0
SebastianMH14 <sebasmh2002@hotmail.com>
2022-04-07 10:15:45 -0500

    Add task 8

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   const arrayToStr = [];
-  if (startString) {
+  if (startString || typeof(startString) !== 'string') {
     for (const value of set) {
       if (value.startsWith(startString)) {
         arrayToStr.push(value.slice(startString.length));
