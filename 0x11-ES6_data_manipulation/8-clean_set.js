9f60031f280ad01536c5d34e19542b1ddc3d1f0d
SebastianMH14 <sebasmh2002@hotmail.com>
2022-04-07 10:23:33 -0500

    Fix task 8

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,12 @@
 export default function cleanSet(set, startString) {
   const arrayToStr = [];
-  if (startString || typeof(startString) !== 'string') {
+  if (startString || typeof (startString) === 'string') {
     for (const value of set) {
       if (value.startsWith(startString)) {
         arrayToStr.push(value.slice(startString.length));
       }
     }
+    return arrayToStr.join('-');
   }
-  return arrayToStr.join('-');
+  return '';
 }
