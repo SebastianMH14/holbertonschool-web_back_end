9ad6e23aa19d4f2fde73392ea5d002ff29de5358
SebastianMH14 <sebasmh2002@hotmail.com>
2022-04-11 22:17:26 -0500

    add task 4

---
+++ 0x12-Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+const app = require('http');
+
+const port = 1245;
+
+const helloWorld = ((req, res) => {
+  res.writeHead(200);
+  res.end('Hello Holberton School!');
+});
+
+const server = app.createServer(helloWorld);
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
