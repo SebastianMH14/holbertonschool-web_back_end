696fa6ba822a3dceffdb3738f2973ed2f17fe851
SebastianMH14 <sebasmh2002@hotmail.com>
2022-08-17 10:45:05 -0500

    Fix task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python3
-'''execute multiple coroutines at the same time with async'''
+""" Tasks """
 
-import asyncio
 from typing import List
+import asyncio
+import random
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
-    ''' Async function that calls task_wait_random n number of times'''
-    delays: List = []
+    """ execute task_wait_random from 3-tasks.py file
+    n times with the specified max_delay
+    Parameters
+    ----------
+    n : int
+      number of times to execute wait_random
+    max_delay : int
+        maximum delay
+    Returns
+    -------
+    list
+        list of all the delays returned from task_wait_random
+    """
+    spawn_list = []
+    delay_list = []
     for i in range(n):
-        delay = task_wait_random(max_delay)
-        delays.append(delay)
-    res_list: List = []
-    for task in asyncio.as_completed(delays):
-        res: float = await task
-        res_list.append(res)
-    return res_list
+        delayed_task = task_wait_random(max_delay)
+        delayed_task.add_done_callback(lambda x: delay_list.append(x.result()))
+        spawn_list.append(delayed_task)
+
+    for spawn in spawn_list:
+        await spawn
+
+    return delay_list
