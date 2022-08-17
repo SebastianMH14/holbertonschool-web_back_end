0c482a2db924a2b8c548e7f656128bbe2b0d2469
SebastianMH14 <sebasmh2002@hotmail.com>
2022-08-17 11:34:55 -0500

    add advanced task

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -7,40 +7,27 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
-from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-
-if getenv('AUTH_TYPE') == 'auth':
+if (os.getenv("AUTH_TYPE") == "auth"):
+    from api.v1.auth.auth import Auth
     auth = Auth()
-elif getenv('AUTH_TYPE') == 'basic_auth':
+elif (os.getenv("AUTH_TYPE") == "basic_auth"):
+    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif getenv('AUTH_TYPE') == 'session_auth':
+elif (os.getenv("AUTH_TYPE") == "session_auth"):
+    from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
-
-
-@app.before_request
-def before_request():
-    """define the authentication method"""
-    if auth is not None:
-        path = request.path
-        check = auth.require_auth(path, [
-            '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
-            '/api/v1/auth_session/login/'])
-        if check is True:
-            if auth.authorization_header(request) is None \
-                    and auth.session_cookie(request) is None:
-                abort(401)
-            if auth.current_user(request) is None:
-                abort(403)
-            else:
-                request.current_user = auth.current_user(request)
+elif (os.getenv("AUTH_TYPE") == "session_exp_auth"):
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+elif (os.getenv("AUTH_TYPE") == "session_db_auth"):
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 
 @app.errorhandler(404)
@@ -51,18 +38,41 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error):
-    """unauthorized handler for erros 401"""
+def un_authorized(error) -> str:
+    """ Unauthorized handler
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error):
-    """forbidden handler for errors 403"""
+def forbidden(error) -> str:
+    """ Forbidden handler
+    """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request_method() -> None:
+    """ method to handler before_request
+    """
+    if (auth is None):
+        return
+    list_excluded = ['/api/v1/status/',
+                     '/api/v1/unauthorized/',
+                     '/api/v1/forbidden/',
+                     '/api/v1/auth_session/login/']
+
+    if not auth.require_auth(request.path, list_excluded):
+        return
+    if not auth.authorization_header(request)\
+       and not auth.session_cookie(request):
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)
